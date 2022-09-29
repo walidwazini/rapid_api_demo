dc4f29b24d24f6eca3d466a6e66fc60361fbe26a
Walid Wazini <wazini25@gmail.com>
2022-09-29 21:31:14 +0800

    first setup

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import { useEffect, useState } from "react";
+
+const App = () => {
+  return (
+    <div className='w-full min-h-screen bg-[#30305c]'>
+      <h1 className='text-3xl font-bold underline'>Hello world!</h1>
+    </div>
+  );
+};
+
+export default App;
